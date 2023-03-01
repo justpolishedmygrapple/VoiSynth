bf503ccb23ebf9425c6b5bf30be897dacfb78c01
justpolishedmygrapple <mangela@oregonstate.edu>
2023-02-28 18:36:04 -0800

    Figured out how to play a single media file from the API

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.myapplication
 
+import android.media.AudioAttributes
+import android.media.MediaPlayer
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.core.net.toUri
+import okhttp3.OkHttpClient
+import org.chromium.net.CronetEngine
+import org.chromium.net.UrlRequest
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import retrofit2.http.Header
+import retrofit2.http.Headers
+import java.util.concurrent.Executor
 
 const val ELEVEN_LABS_API = BuildConfig.ELEVEN_LABS_API
 
@@ -15,13 +25,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
     }
 
     override fun onResume() {
         super.onResume()
         val serviceTest = service.search().enqueue(
-            object: Callback<String>{
+            object : Callback<String> {
                 override fun onResponse(call: Call<String>, response: Response<String>) {
                     Log.d("apicall", response.body().toString())
                 }
@@ -31,6 +40,34 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         )
+
+        //        Testing playing a music file streamed from the internet... works.
+        //Now, how to apply headers?
+
+
+
+        //Simply playing one single audio file in the main activity for now
+        val header = HashMap<String,String>()
+
+        header["xi-api-key"] = ELEVEN_LABS_API
+
+        val url = urlSchemeBuilder("HwVUitQTLIxLzWQTCkhA")
+
+        val uri = Uri.parse(url)
+
+        val mediaPlayer = MediaPlayer()
+
+        val context = this
+        mediaPlayer.setDataSource(this, Uri.parse(url), header)
+        mediaPlayer.prepare()
+        mediaPlayer.start()
     }
+
+
+    //Function that will be useful later to dynamically get the path to a particular audio file
+    private fun urlSchemeBuilder(audioId: String): String{
+        return "https://api.elevenlabs.io/v1/history/${audioId}/audio"
+    }
+
 
 }
