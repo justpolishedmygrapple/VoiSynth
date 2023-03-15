f3a08c5a80e2550ba2457c61f0831debda52974b
justpolishedmygrapple <mangela@oregonstate.edu>
2023-03-15 16:36:01 -0700

    Removed fragments, updated AndroidManifest.xml

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,40 +1,17 @@
 package com.example.myapplication
 
-import android.content.Intent
-import android.content.res.Configuration
 import android.media.MediaPlayer
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
-import android.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.R.*
-import com.example.myapplication.data.TextToVoiceQuery
-import com.example.myapplication.data.Voice
-import com.example.myapplication.data.VoiceAdapter
-import com.example.myapplication.data.VoiceResponse
-import com.example.myapplication.ui.HistoryViewActivity
-import com.example.myapplication.ui.VoiceInterface
-import com.example.myapplication.ui.VoiceListActivity
-import com.squareup.moshi.JsonAdapter
-import com.squareup.moshi.Moshi
-import okhttp3.ResponseBody
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import java.io.File
-import kotlin.io.path.writeBytes
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupWithNavController
-import com.example.myapplication.fragments.HistoryBySelectedVoiceFragment
 import com.google.android.material.navigation.NavigationView
 
 const val ELEVEN_LABS_API = BuildConfig.ELEVEN_LABS_API
--- app/src/main/java/com/example/myapplication/fragments/QuickGenerateFragment.kt
+++ app/src/main/java/com/example/myapplication/fragments/QuickGenerateFragment.kt
@@ -1,14 +1,9 @@
 package com.example.myapplication.fragments
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.myapplication.R
-import com.example.myapplication.ui.HistoryViewActivity
-import com.example.myapplication.ui.VoiceListActivity
 
 class QuickGenerateFragment: Fragment(R.layout.quick_generate) {
 
