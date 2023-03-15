ff7f44cf2aefaa58857c9e738ca86fff7ff6f4e1
justpolishedmygrapple <mangela@oregonstate.edu>
2023-03-15 11:42:52 -0700

    Jetpack navigation implemented with drawer.

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,6 +7,12 @@ import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.R.*
 import com.example.myapplication.data.TextToVoiceQuery
@@ -24,15 +30,52 @@ import retrofit2.Callback
 import retrofit2.Response
 import java.io.File
 import kotlin.io.path.writeBytes
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 
 const val ELEVEN_LABS_API = BuildConfig.ELEVEN_LABS_API
 
 
 class MainActivity : AppCompatActivity() {
 
+
+    private lateinit var appBarConfig: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(layout.activity_main)
+
+
+        var toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(
+                R.id.nav_host_fragment
+            ) as NavHostFragment
+        val navController = navHostFragment.navController
+        appBarConfig = AppBarConfiguration(navController.graph)
+
+        val drawerLayout: DrawerLayout =
+            findViewById(R.id.drawer_layout)
+        appBarConfig =
+            AppBarConfiguration(navController.graph, drawerLayout)
+
+        findViewById<NavigationView>(R.id.nav_view)
+            ?.setupWithNavController(navController)
+
+        setupActionBarWithNavController(
+            navController,
+            appBarConfig
+        )
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfig) || super.onSupportNavigateUp()
     }
 
 }
